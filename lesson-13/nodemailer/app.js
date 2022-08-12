de1da26547a809c94824e69efc1d165ce13c06da
MandorSavall <bogdan.lyamzin.d@gmail.com>
2022-08-12 23:10:48 +0300

    add lesson

--- lesson-13/nodemailer/app.js
+++ lesson-13/nodemailer/app.js
@@ -1,27 +1,27 @@
-const nodemailer = require("nodemailer");
-require("dotenv").config();
+const nodemailer = require('nodemailer')
 
-const {EMAIL_PASSWORD} = process.env;
+require('dotenv').config()
 
-const nodemailerConfig = {
-    host: "smtp.meta.ua", 
-    port: 465, // 25, 465, 2255
-    secure: true,
-    auth: {
-        user: "bogdan.lyamzin@meta.ua",
-        pass: EMAIL_PASSWORD
-    }
-};
+const config = {
+  host: 'smtp.meta.ua',
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'goitnodejs@meta.ua',
+    pass: process.env.PASSWORD,
+  },
+}
 
-const transporter = nodemailer.createTransport(nodemailerConfig);
+const transporter = nodemailer.createTransport(config)
+const emailOptions = {
+  from: 'goitnodejs@meta.ua',
+  to: 'bogdan.lyamzin.d@gmail.com',
+  subject: 'Nodemailer test',
+  text: 'Привет. Мы тестируем отправку писем!',
+  html: 'Привет. Мы тестируем отправку писем!',
+}
 
-const email = {
-    to: "bogdan.lyamzin.d@gmail.com",
-    from: "bogdan.lyamzin@meta.ua",
-    subject: "Регистрация на сайте",
-    html: "<p>Поздравляем с успешной регистрацией на нашем сайте</p>"
-};
-
-transporter.sendMail(email)
-    .then(()=> console.log("Email success send"))
-    .catch(error => console.log(error.message));
+transporter
+  .sendMail(emailOptions)
+  .then((info) => console.log(info))
+  .catch((err) => console.log(err))
